757de610a059f940763349b0185de907e1eeae76
v1gor <arkhat97@gmail.com>
2021-06-10 13:57:53 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# ==== 0 урок
+# print('Hello World')
+# === end 0
+
+# 1 урок комменты
+
+# программа которая печатает привет
+"""
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+"""
+# print('asd')
+
+# end 1 
+
+
+# урок 2
+
+# box = 'Masha' # string str
+# box = "123asdasd"
+# PersonName = 'Arkhat'
+# Person_age = 24 # integer int
+# massa123 = 2.23 # float
+# isEmpty = False # boolean bool 
+# # print(box)
+# print(type(box))
+
+# урок 3
+
+# word = '123'
+# word = int(word) + 10
+
+# print(type(word))
+# print(word)
+# name = "Arkhat" #str
+age = 10
+str_age = str(age)
+print(type(str_age))
+fl = 123.23 + 10
+print(round(fl, 2))
